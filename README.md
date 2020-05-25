98b6967244cd112afe676474f3ec44e012103c81
Ralf Ebert <info@ralfebert.de>
2020-05-25 19:11:26 +0200

    CalendarDate initial implementation

---
+++ README.md
@@ -0,0 +1,4 @@
+# CalendarDate
+
+CalendarDate is a Swift type that represents a Date as year, month and day value.
+Includes support for formatting as a 'yyyy-mm-dd' string and JSON coding as such a string out of the box.
---
+++ Sources/CalendarDate/CalendarDate.swift
@@ -0,0 +1,93 @@
+// MIT License
+//
+// Copyright (c) 2020 Ralf Ebert
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Foundation
+
+/**
+ CalendarDate is a Swift type that represents a Date as year, month and day value.
+ Includes support for formatting as a 'yyyy-mm-dd' string and JSON coding as such a string out of the box.
+ */
+public struct CalendarDate: LosslessStringConvertible, Codable, Equatable, Hashable {
+
+    public let year, month, day: Int
+
+    public init() {
+        self.init(date: Date())
+    }
+
+    public init?(_ description: String) {
+        if let date = Self.formatter.date(from: description) {
+            self.init(date: date)
+        } else {
+            return nil
+        }
+    }
+
+    public init(year: Int, month: Int, day: Int) {
+        self.year = year
+        self.month = month
+        self.day = day
+    }
+
+    public init(date: Date) {
+        let calendar = Calendar.current
+        self.year = calendar.component(.year, from: date)
+        self.month = calendar.component(.month, from: date)
+        self.day = calendar.component(.day, from: date)
+    }
+
+    public var date: Date {
+        DateComponents(calendar: Calendar.current, year: self.year, month: self.month, day: self.day).date!
+    }
+
+    public var description: String {
+        Self.formatter.string(from: self.date)
+    }
+
+    private static let formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+
+    // MARK: - JSON coding
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let string = try container.decode(String.self)
+
+        guard let value = CalendarDate(string) else {
+            throw DecodingError.dataCorruptedError(
+                in: container,
+                debugDescription: "Not a valid calendar date: \"\(string)\""
+            )
+        }
+
+        self = value
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(self.description)
+    }
+
+}
